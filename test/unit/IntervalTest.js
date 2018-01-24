36f9487fbeac26c49ca7316dafaed9f46ed3a8cd
samyl <irosam91@gmail.com>
2018-01-24 14:38:25 +0100

    updated test files

--- test/unit/IntervalTest.js
+++ test/unit/IntervalTest.js
@@ -1,26 +1,10 @@
-/*describe("Interval - overlapping", function () {
-    testedInterval = new Interval(10, 20);
-
-    [
-        new Interval(8, 12),
-        new Interval(15, 16),
-        new Interval(17, 22),
-        new Interval(10, 20),
-        new Interval(8, 21)
-
-    ].forEach(function (interval) {
-        it("should overlaps " + testedInterval.toString() + " and " + interval.toString(), function () {
-            expect(testedInterval.overlaps(interval)).toBeTruthy();
+describe("Includes", function()
+{
+    Interval.start = 1;
+    Interval.end = 12;
+    var intervalle2 = {start : 4, end : 6};
+        it("should returns true when intervalle2 is in intervalle1", function () {
+            var result = Interval.prototype.includes(intervalle2);
+            expect(result).toBeTruthy();
         });
-    });
-
-    [
-        new Interval(8, 9),
-        new Interval(21, 22)
-
-    ].forEach(function (interval) {
-        it("should not overlaps " + testedInterval.toString() + " and " + interval.toString(), function () {
-            expect(testedInterval.overlaps(interval)).toBeFalsy();
-        });
-    });
-});*/
+})
