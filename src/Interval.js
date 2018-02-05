3bb6092d2fd287df687d2cd1fb154160290f916f
samyl <irosam91@gmail.com>
2018-02-05 14:15:14 +0100

    added union and its tests

--- src/Interval.js
+++ src/Interval.js
@@ -23,6 +23,8 @@ Interval.prototype.overlaps = function (interval) {
  * @returns {boolean}
  */
 Interval.prototype.includes = function (interval) {
+	
+	return this.start <= interval.start && interval.start <= this.end && this.start <= interval.end && interval.end <= this.end;
 
 };
 
@@ -32,6 +34,18 @@ Interval.prototype.includes = function (interval) {
  * @returns {Interval[]}
  */
 Interval.prototype.union = function (interval) {
+	
+	if (this.end < interval.start) {
+		return [this, interval];
+	} else if (interval.end < this.start) {
+		return [interval, this];
+	}
+	
+	var start, end;
+	start = this.start < interval.start ? this.start : interval.start;
+	end = this.end > interval.end ? this.end : interval.end;
+	
+return new Interval(start, end);
 
 };
 
--- test/unit/IntervalTest.js
+++ test/unit/IntervalTest.js
@@ -1,4 +1,4 @@
-/*describe("Interval - overlapping", function () {
+describe("Interval - overlapping", function () {
     testedInterval = new Interval(10, 20);
 
     [
@@ -23,4 +23,74 @@
             expect(testedInterval.overlaps(interval)).toBeFalsy();
         });
     });
-});*/
+});
+
+describe("Interval - including", function () {
+    testedInterval = new Interval(10, 20);
+
+    [
+        
+        new Interval(15, 18),
+        new Interval(13, 18)
+     
+    ].forEach(function (interval) {
+        it("should includes " + testedInterval.toString() + " and " + interval.toString(), function () {
+            expect(testedInterval.includes(interval)).toBeTruthy();
+        });
+});
+
+[
+		new Interval(7, 11),
+		new Interval(19, 21),
+        new Interval(8, 9)
+	
+    ].forEach(function (interval) {
+        it("should not includes " + testedInterval.toString() + " and " + interval.toString(), function () {
+            expect(testedInterval.includes(interval)).toBeFalsy();
+        });
+    });
+});
+
+describe("Interval - union", function () {
+    testedInterval = new Interval(10, 20);
+
+    [
+		{
+			interval : new Interval(6, 13),
+			result : new Interval(6,20)
+		},
+		{
+			interval : new Interval(12, 21),
+			result : new Interval(10,21)
+		},
+		{
+			interval : new Interval(8, 9),
+			result : [new Interval(8,9), testedInterval]
+		},
+		{
+			interval : new Interval(6, 23),
+			result : new Interval(6,23)
+		},
+		{
+			interval : new Interval(21, 22),
+			result : [testedInterval, new Interval(21,22)]
+		}
+		
+		
+    ].forEach(function (interval) {
+		var msg = "";
+		if (typeof interval.result.start !== 'undefined')
+			msg = "should returns Interval[" + interval.result.start + ", " + interval.result.end + "] when doing union of " + testedInterval.toString() + " and " + interval.interval.toString();
+		else
+			msg = "should returns Interval(" + interval.result + ") when doing union of " + testedInterval.toString() + " and " + interval.interval.toString();
+        it(msg, function () {
+            var result = testedInterval.union(interval.interval);
+			if (typeof interval.result.start !== 'undefined') {
+				expect(result.start).toEqual(interval.result.start);
+				expect(result.end).toEqual(interval.result.end);
+			} else
+				expect(result).toEqual(interval.result);
+			
+        });
+    });
+});
